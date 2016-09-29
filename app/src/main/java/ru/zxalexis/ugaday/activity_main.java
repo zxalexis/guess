0c63a5e7ef00a8e65462644ab15405a123a27434
Alexey Khromov <zxalexis@gmail.com>
2016-09-30 02:47:48 +0400

    fixed French translation

--- app/src/main/java/ru/zxalexis/ugaday/activity_main.java
+++ app/src/main/java/ru/zxalexis/ugaday/activity_main.java
@@ -2,6 +2,7 @@ package ru.zxalexis.ugaday;
 
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.*;
@@ -9,64 +10,62 @@ import java.util.Random;
 import android.view.WindowManager;
 
 
-
 public class activity_main extends Activity {
 	TextView tvInfo;
 	EditText etInput;
-	Button bControl;	
+	Button bControl;
 	Integer iPopitka;
 	Boolean bFinished;
 	Integer iChislo, iVvod;
-	
+	Random Rn;
+
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_layout);
-		
-		tvInfo = (TextView)findViewById(R.id.textView1);
-		etInput = (EditText)findViewById(R.id.editText1);
-		bControl = (Button)findViewById(R.id.button1);
-		
-		iChislo = new Random().nextInt(100) + 1;
+
+		tvInfo = (TextView) findViewById(R.id.textView1);
+		etInput = (EditText) findViewById(R.id.editText1);
+		bControl = (Button) findViewById(R.id.button1);
+		Rn = new Random();
+		iChislo = Rn.nextInt(100) + 1;
+		if (iChislo > 100) iChislo -= 100;
 		iPopitka = 0;
 		bFinished = false;
 		
 		 /* code to show keyboard on startup.this code is not working.*/
 		getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
-		
 	}
-	
+
 	public void onClick(View v) {
-		if (bFinished == false) {
+		if !bFinished  {
 			if (!etInput.getText().toString().isEmpty()) {
 				iVvod = Integer.parseInt(etInput.getText().toString());
 			} else {
 				iVvod = 0;
-			};
+			}
+
 			if ((iVvod > 100) | (iVvod < 1)) {
 				tvInfo.setText(getResources().getString(R.string.error));
 				etInput.setText("");
 			} else {
-				
+
 				if (iVvod > iChislo) {
-					iPopitka = iPopitka+1;
+					iPopitka = iPopitka + 1;
 					tvInfo.setText(getResources().getString(R.string.ahead));
-				} else 
-		
-				if (iVvod < iChislo) {
-					iPopitka = iPopitka+1;
+				} else if (iVvod < iChislo) {
+					iPopitka = iPopitka + 1;
 					tvInfo.setText(getResources().getString(R.string.behind));
-				} else
-					
-				if (iVvod == iChislo) {
+				} else if (iVvod == iChislo) {
 					iPopitka = 0;
 					bFinished = true;
 					tvInfo.setText(getResources().getString(R.string.hit));
 					bControl.setText(getResources().getString(R.string.playmore));
 				}
-				
+
 				etInput.setText("");
-				
+
 				if ((iPopitka == 8) & (!bFinished)) {
 					bFinished = true;
 					tvInfo.setText(getResources().getString(R.string.fool));
@@ -77,11 +76,12 @@ public class activity_main extends Activity {
 			etInput.setText("");
 			tvInfo.setText(getResources().getString(R.string.try_to_guess));
 			bControl.setText(getResources().getString(R.string.input_value));
-			iChislo = new Random().nextInt(100) + 1;
+			iChislo = Rn.nextInt(100) + 1;
+			if (iChislo > 100) iChislo -= 100;
 			bFinished = false;
 			iPopitka = 0;
 		}
-		
+
 	} //onClick
-	
+
 }// activity_main
